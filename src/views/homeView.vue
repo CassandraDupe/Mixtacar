2a71df289c61fa781bb01d5349dfc6af54437c46
Coline <colineauber@yahoo.fr>
2023-03-10 14:19:58 +0100

    test_1

---
+++ src/views/homeView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="about">
+      <h1></h1>
+    </div>
+  </template>
+  
+  <style scoped>
+  .about {
+    min-height: 100vh;
+    text-align: center;
+  }
+  </style>
+  
+  <script setup>
+  </script>
