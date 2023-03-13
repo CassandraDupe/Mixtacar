ec4a12b5e9a80fadc2773b7c2d3bfc6c72052139
Coline <colineauber@yahoo.fr>
2023-03-13 08:51:06 +0100

    home_page_v1

---
+++ src/components/homeComp.vue
@@ -0,0 +1,34 @@
+<template>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+export default {
+  data: () => ({
+    depart: '',
+    lieuRules: [
+      value => {
+        //if (value?.length > 3) return true
+        //return 'lieu must be at least 3 characters.'
+
+        return true
+      },
+    ],
+    arrivee: '',
+    lieuRules: [
+      value => {
+          return true
+
+      },
+    ],
+    date: '',
+    dateRules: [
+      value => {
+         return true
+      },
+    ],
+  }),
+}
+</script>
--- src/views/homeView.vue
+++ src/views/homeView.vue
@@ -1,15 +1,46 @@
 <template>
-    <div class="about">
-      <h1></h1>
-    </div>
-  </template>
+  <div class="about">
+    <h1>This is the home page</h1>
+  </div>
+  <div id="form">
+    <v-sheet width="300" class="mx-auto">
+      <v-form fast-fail @submit.prevent>
+        <v-text-field v-model="depart" type="text" label="Départ"></v-text-field>
+        <!-- <v-text-field v-model="firstName" label="Départ" :rules="firstNameRules"></v-text-field>  -->
+
+        <v-text-field v-model="arrivee" type="text" label="Arrivée"></v-text-field>
+
+        <v-text-field v-model="date" type="date" label="Date"></v-text-field>
+
+        <v-btn type="submit" block class="mt-2">Rechercher</v-btn>
+      </v-form>
+    </v-sheet>
+  </div>
+
+  <br>
+
+  <v-btn class="milieu">
+    Besoin piéton
+  </v-btn>
+
+  <br><br>
+
+  <v-btn class="milieu">
+    Proposition trajet
+  </v-btn>
+</template>
   
-  <style scoped>
-  .about {
-    min-height: 100vh;
-    text-align: center;
-  }
-  </style>
+<style scoped>
+.milieu {
+  align-items: center;
+}
+
+#form {
+  size-adjust: 25%;
+}
+</style>
   
-  <script setup>
-  </script>
+<script setup>
+import trajet from "../trajet.js";
+import homeComp from "../components/homeComp.vue"
+</script>
